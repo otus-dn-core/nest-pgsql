6facd807df352791e18ce0f17598d9131dba2926
otus-dn-core <otus@dn-core.ru>
2021-08-03 10:14:09 +0300

    user pass + jwt, before validation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/createUser.dto";
 import { UserEntity } from "./user.entity";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "@app/config";
+import { UserResponseInterface} from './types/userResponse.interface'
 
 @Injectable()
 export class UserService {
@@ -13,7 +16,26 @@ export class UserService {
     async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
         const newUser = new UserEntity();
         Object.assign(newUser, createUserDto);
-        console.log('newUser', newUser); 
         return await this.userRepository.save(newUser);
+    }
+
+    generateJwt(user: UserEntity): string {
+        return sign(
+            {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            },
+            JWT_SECRET,
+        );
+    }
+
+    buildUserResponse(user: UserEntity): UserResponseInterface {
+       return {
+           user: {
+               ...user, // spread - все поля пользователя
+               token: this.generateJwt(user)
+           }
+       } 
     }
 }
