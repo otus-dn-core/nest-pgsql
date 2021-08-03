d61facb6f212f5dece421185c0f66ed423606553
otus-dn-core <otus@dn-core.ru>
2021-08-03 08:38:33 +0300

    before user to PG

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,6 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
     @Post('users')
     async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
-        console.log('createUserDto', createUserDto);
         return this.userService.createUser(createUserDto);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { UserEntity } from "./user.entity";
 
 @Injectable()
 export class UserService {
     async createUser(createUserDto: CreateUserDto) {
+        const newUser = new UserEntity();
+        Object.assign(newUser, createUserDto);
+        console.log('newUser', newUser); 
         return createUserDto;
     }
 }
