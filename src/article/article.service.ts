c025c1a38d7eb9c70452e9d7b416d11f9840d3e3
otus-dn-core <otus@dn-core.ru>
2021-08-04 21:10:05 +0300

    Create article, before End with article

---
+++ src/article/article.service.ts
@@ -0,0 +1,32 @@
+import { UserEntity } from "@app/user/user.entity";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ArticleEntity } from "./article.entity";
+import { CreateArticleDto } from "./dto/createArticle.dto";
+
+@Injectable()
+export class ArticleService {
+    constructor(
+        @InjectRepository(ArticleEntity) 
+        private readonly articleRepository: Repository<ArticleEntity>,
+    ) {}
+
+    async createArticle(
+        currentUser: UserEntity, 
+        createArticleDto: CreateArticleDto
+    ): Promise<ArticleEntity> {
+        const article = new ArticleEntity();
+        Object.assign(article, createArticleDto);
+
+        if (!article.tagList) {
+            article.tagList = [];
+        }
+
+        article.slug = 'foo'; // временная заглушка. (Генерируется на сервере)
+
+        article.author = currentUser; // TypeORM сам возмёт id пользователя
+
+        return await this.articleRepository.save(article);
+    }
+}
