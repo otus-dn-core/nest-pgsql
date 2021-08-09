bbbe6385f46ea782ed594702b1c3471bdf08b4dc
otus-dn-core <otus@dn-core.ru>
2021-08-09 22:13:20 +0300

    Delete Articles

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,7 @@
 import { User } from "@app/user/decorators/user.decorator";
 import { AuthGuard } from "@app/user/guards/auth.guard";
 import { UserEntity } from "@app/user/user.entity";
-import { Body, Get, Param, Query, UseGuards } from "@nestjs/common";
+import { Body, Delete, Get, Param, Query, UseGuards } from "@nestjs/common";
 import { Controller, Post } from "@nestjs/common";
 import { ArticleService } from "./article.service";
 import { CreateArticleDto } from './dto/createArticle.dto';
@@ -38,4 +38,14 @@ export class ArticleController {
     const article = await this.articleService.findBySlug(slug);
     return this.articleService.buildArticleResponse(article);
   }
+
+  @Delete(':slug')
+  @UseGuards(AuthGuard)
+  async deleteArticle(
+    @User('id') currentUserId: number,
+    @Param('slug') slug: string,
+  ) {
+    return await this.articleService.deleteArticle(slug, currentUserId);
+  }
+
 }
